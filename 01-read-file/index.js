050a988bb163d5cad3040c9f7333f18bfebf85a7
VoronovY <voronov.yuri87@gmail.com>
2023-05-08 15:45:32 +0200

    refactor: 01-read-file add 'end' and 'error' listeners

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,13 @@
-const fs = require('node:fs');
-const path = require('path');
-const process = require('process');
+const fs = require("fs");
+const path = require("path");
+const process = require("process");
 
-const textPath = path.join(__dirname, 'text.txt');
-const readableStream = fs.createReadStream(textPath, 'UTF-8');
-readableStream.on('data', chunk => process.stdout.write(chunk));
+const textPath = path.join(__dirname, "text.txt");
+const readableStream = fs.createReadStream(textPath, "UTF-8");
+
+readableStream.on("data", (chunk) => process.stdout.write(chunk));
+readableStream.on("end", () => process.stdout.write("\n"));
+readableStream.on("error", (err) => {
+  process.stdout.write("При выполнении кода возникла ошибка:\n");
+  process.stdout.write(err.toString());
+});
